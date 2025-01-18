45961fc8ce12f260322888bc621c2b627614413d
helauren <helauren42@gmail.com>
2025-01-18 08:06:03 +0100

    install minor change

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ rm -rf $INSTALLOC
 mkdir -p $BINLOC
 mkdir $INSTALLOC
 
-cp ./manager.py ./bg-slideshow.py ./database.py ./requirements.txt ./uninstall.sh $INSTALLOC
+cp ./manager.py ./bg-slideshow.py ./database.py ./uninstall.sh $INSTALLOC
 cp ./bg-slideshow ./bg-slideshow.sh "$HOME/.local/bin/"
 
 chown $USER:$USER $INSTALLOC
